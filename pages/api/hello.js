60e9dbd7aa432ce4ecab357aa5b05f967cb35d5a
Nicole Hörner <nicole.hoerner@gmail.com>
2023-08-17 11:08:32 +0200

    add fetch function and useEffect for API integration

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  const response = await fetch(
+    "http://api.aviationstack.com/v1/flights?access_key=eef146cc9441c581c88b1ba9363a5d4b"
+  );
+
+  const data = await response.json();
+  res.status(200).json({ data: data });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from "next/head";
 import styled from "styled-components";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    async function getFlights() {
+      const res = await fetch("/api/hello");
+      const data = await res.json();
+      console.log(data);
+    }
+    getFlights();
+  }, []);
+
   return (
     <>
       <Head>
