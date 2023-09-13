e4d02f0615c829523271de653b911202b9076ff1
Nicole Hörner <nicole.hoerner@gmail.com>
2023-09-13 09:54:44 +0200

    Revert "add form component, add uselocalStorageState"
    This reverts commit f20ce0e45bf889a5753b90ddf4e8a93e2fec9e11.

--- pages/flight_list.js
+++ pages/flight_list.js
@@ -3,35 +3,15 @@ import { FlightNumber } from "../components/StyledComponents/StyledFlightNumber"
 import { flights } from "../data/septemberFlights";
 import { useState } from "react";
 import AddFlightForm from "../components/Form/AddFlightForm";
-import useLocalStorageState from "use-local-storage-state";
+import { useLocalStorageState } from "use-local-storage-state";
 
 export default function FlightList() {
+  const router = useRouter();
   const [showForm, setShowForm] = useState(false);
-  const [flightsList, setFlightsList] = useLocalStorageState("flights", {
-    defaultValue: [],
-  });
-
-  function addFlight(newFlight) {
-    const updatedFlights = [...flightsList, newFlight].sort((a, b) => {
-      const dateA = new Date(
-        a.scheduled_date.year,
-        a.scheduled_date.month, // I might need to convert month names to numbers?
-        a.scheduled_date.day
-      );
-      const dateB = new Date(
-        b.scheduled_date.year,
-        b.scheduled_date.month, // I might need to convert month names to numbers?
-        b.scheduled_date.day
-      );
-      return dateA - dateB;
-    });
-    // Update the sorted flights list in localStorage and state
-    setFlightsList(updatedFlights);
-  }
 
   const handleAddFlightClick = () => {
     // Navigate to the form when the button is clicked
-    setShowForm(true);
+    router.push("/flight-form");
   };
   return (
     <>
@@ -64,11 +44,7 @@ export default function FlightList() {
           </FlightRow>
         ))}
       </ScheduledList>
-      {showForm ? (
-        <AddFlightForm onAddFlight={addFlight} />
-      ) : (
-        <Button onClick={handleAddFlightClick}>Add Flight</Button>
-      )}
+      <Button onClick={handleAddFlightClick}>Add Flight</Button>
     </>
   );
 }
