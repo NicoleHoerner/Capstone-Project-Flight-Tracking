c7381029ab3a6a7b455bb23fa7c7c8c766b7b49d
Nicole Hörner <nicole.hoerner@gmail.com>
2023-09-14 19:41:14 +0200

    add validation for flight #

--- components/Form/AddFlightForm.js
+++ components/Form/AddFlightForm.js
@@ -7,6 +7,119 @@ import {
   InputBox,
   InputField,
 } from "./AddFlightForm.styled";
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+import { flights } from "@/data/septemberFlights";
+
+export default function AddFlightForm() {
+  const [flightsOfInterest, setFlightsOfInterest] = useLocalStorageState(
+    "flights",
+    { defaultValue: flights }
+  );
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleFocus = () => setIsFocused(true);
+  const handleBlur = () => setIsFocused(false);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const flightData = Object.fromEntries(formData);
+    const date = new Date(flightData.scheduled_date);
+    flightData.scheduled_date = {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      day: date.getDate(),
+    };
+
+    setFlightsOfInterest([...flightsOfInterest, flightData]);
+    event.target.reset();
+    event.target.flight_iata.focus();
+  }
+
+  return (
+    <>
+      <StyledForm onSubmit={handleSubmit}>
+        <Card>
+          <StyledHeading>Schedule your next flight</StyledHeading>
+          <InputBox>
+            <InputLabel htmlFor="flight_iata" isFocused={isFocused}>
+              Flight #
+            </InputLabel>
+            <InputField
+              id="flight_iata"
+              type="text"
+              name="flight_iata"
+              required
+              maxLength="7"
+              pattern="^[A-Za-z]{2,3}\d{1,4}[A-Za-z]?$"
+              onFocus={handleFocus}
+              onBlur={(event) => {
+                handleBlur(event);
+                const input = event.target;
+                if (!input.validity.valid) {
+                  input.setCustomValidity(
+                    "Flight number must be 2 or 3 letters followed by 1 to 4 digits with an optional letter."
+                  );
+                } else {
+                  input.setCustomValidity(""); // Reset custom validity
+                }
+              }}
+            />
+          </InputBox>
+          <InputBox>
+            <InputLabel htmlFor="scheduled_date" isFocused={isFocused}>
+              Date of Flight
+            </InputLabel>
+            <InputField
+              id="scheduled_date"
+              type="date"
+              name="scheduled_date"
+              required
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </InputBox>
+          <InputBox>
+            <InputLabel htmlFor="departure" isFocused={isFocused}>
+              Departure
+            </InputLabel>
+            <InputField
+              id="departure"
+              type="text"
+              name="departure"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </InputBox>
+          <InputBox>
+            <InputLabel htmlFor="arrival" isFocused={isFocused}>
+              Arrival
+            </InputLabel>
+            <InputField
+              id="arrival"
+              type="text"
+              name="arrival"
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </InputBox>
+          <StyledAddButton type="submit">ADD FLIGHT</StyledAddButton>
+        </Card>
+      </StyledForm>
+    </>
+  );
+}
+
+/* import {
+  StyledForm,
+  Card,
+  StyledAddButton,
+  StyledHeading,
+  InputLabel,
+  InputBox,
+  InputField,
+} from "./AddFlightForm.styled";
 import useLocalStorageState from "use-local-storage-state";
 import { useRouter } from "next/router";
 import { useState } from "react";
@@ -18,6 +131,7 @@ export default function AddFlightForm() {
     { defaultValue: flights }
   );
   const router = useRouter();
+
   const [isFocused, setIsFocused] = useState(false);
   const [isValid, setIsValid] = useState(false);
 
@@ -98,7 +212,7 @@ export default function AddFlightForm() {
       </StyledForm>
     </>
   );
-}
+} */
 
 /* import {
   StyledForm,
